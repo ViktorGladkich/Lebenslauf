c0c5b8c6636b2dd80a230701ca5b84c8a3de8dae
Viktor.Gladkich <viktorgladkich7@gmail.com>
2024-12-12 14:59:35 +0100

    added animation to the button

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
+const button = document.querySelector(".button");
+
+button.addEventListener("click", () => {
+  button.classList.add("active");
+
+  downloadCV();
+  
+  setTimeout(() => {
+    button.classList.remove("active");
+    button.querySelector("i").classList.replace("fa-file-download", "fa-check-circle");
+  }, 900); 
+});
+
 function downloadCV() {
-    const link = document.createElement("a");
-    link.href = "assets/Lebenslauf - Viktor Gladkich.pdf"; 
-    link.download = "Lebenslauf.pdf"; 
-    link.click();
-  }
+  const link = document.createElement("a");
+  link.href = "assets/Lebenslauf - Viktor Gladkich.pdf"; 
+  link.download = "Lebenslauf.pdf"; 
+  link.click(); 
+}
